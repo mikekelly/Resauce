75d5b5f3d59634afa91a3462733b79ac9d20d6a0
Mike Kelly <mike@mykanjo.co.uk>
2009-10-05 10:17:24 +0100

    Added Controller folder and Abstract Resource Controller
    This adds the Resource Controller. This controller is intended to be implemented without an indexAction.
    Routes can then be passed from the front controller with no action specified. This will cause the __call() function to be invoked.
    The __call() function checks the incoming request method (GET/PUT/POST/DELETE/HEAD/OPTIONS) and invokes the appropriate action.
    If an action for a particular request method is not implemented the server will respond with a 405 Method Not Supported - This response also includes an Allow header which by deafult specifies all HTTP methods. The allow header value can be changed by over-riding the value of $allow.

---
+++ Controller/Resource.php
@@ -0,0 +1,57 @@
+<?php
+
+abstract class Resauce_Controller_Resource extends Zend_Controller_Action
+{
+	private $allow = 'GET, PUT, POST, DELETE, HEAD, OPTIONS';
+
+	public function notAllowedAction() {
+		$this->getResponse()->setHeader('Allow', $this->allow);
+		$this->getResponse()->setHttpResponseCode(405);
+		
+		$this->_helper->json(array(
+			'error' => array(
+				'message' => 'Method Not Supported',
+				'statuscode' => 405
+				)
+			)
+		);	
+	}
+
+	public function getAction() {
+		$this->notAllowedAction();
+	}
+
+	public function putAction() {
+		$this->notAllowedAction();
+	}
+
+	public function postAction() {
+		$this->notAllowedAction();
+	}
+
+	public function deleteAction() {
+		$this->notAllowedAction();
+	}
+
+	public function headAction() {
+		$this->notAllowedAction();
+	}
+
+	public function optionsAction() {
+		$this->notAllowedAction();
+	}
+
+	public function __call($method, $args)
+	{
+		//$httpMethod = $this->getRequest()->getMethod();
+		
+		$action = strtolower($this->getRequest()->getMethod()) . 'Action';
+
+		if (method_exists($this, $action)) {
+			return call_user_func_array(array($this, $action), $args);
+		}
+
+		$this->notAllowedAction();
+	}
+
+}
