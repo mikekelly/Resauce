e8b0644d051f8fabdde1b25ec6da719268dc025a
Mike Kelly <mike@mykanjo.co.uk>
2009-10-08 00:50:36 +0100

    Added addResauceRoutes function to Bootstrap
    Routes can now be added from the Bootstrap by passing an array of pattern/controller pairs
    i.e. in Bootstrap:
    $this->addResauceRoutes(array(
    	'blogposts' => 'blogposts',
    	'blogposts/:blogpost' => 'blogpost'
    ));

--- Application/Bootstrap.php
+++ Application/Bootstrap.php
@@ -6,18 +6,27 @@ class Resauce_Application_Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 	public function _initResauceFramework() {
 		// NotFoundController will handle all URIs by default - Special resource controller for returning 404 response
 		Zend_Controller_Front::getInstance()->setDefaultControllerName('not-found');
-		$router = Zend_Controller_Front::getInstance()->getRouter();
-		// Get rid of default routing; take control of your Resauces :-)
-		$router->removeDefaultRoutes();
+		// Remove default routes
+		Zend_Controller_Front::getInstance()->getRouter()->removeDefaultRoutes();
 		// Add route for root URI
-		$router->addRoute(
-			'index',
-			new Zend_Controller_Router_Route(
-				'/',
-				array(
-					'controller' => 'index'
-				)
-			)
-		);
+		$this->addResauceRoutes(array(
+			'/' => 'index'
+		));
 	}
+
+	public function addResauceRoutes($routes) {
+		$router = Zend_Controller_Front::getInstance()->getRouter();
+		foreach ($routes as $pattern => $controller) {
+			$router->addRoute(
+                        	$controller,
+                        	new Zend_Controller_Router_Route(
+                                	$pattern,
+                                	array(
+                                        	'controller' => $controller
+                                	)
+                        	)
+                	);
+		}
+	}
+
 }
