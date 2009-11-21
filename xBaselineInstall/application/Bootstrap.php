70c72ab687a438c758ad2cab27971cd0ae995d83
mike <mike@mike-laptop.(none)>
2009-11-21 18:53:50 +0000

    Removed baseline install - will move this into a project of its own

--- xBaselineInstall/application/Bootstrap.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-class Bootstrap extends Resauce_Application_Bootstrap {
-
-}
--- xBaselineInstall/application/controllers/NotFoundController.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class NotFoundController extends Resauce_Controller_Resource
-{
-	public function __call($method, $args) {
-		$this->getResponse()->setHttpResponseCode(404);
-		$this->render('not-found');
-	}
-}
